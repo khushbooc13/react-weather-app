f06d8e0872bf8094cc7b630137dfb677b41e2ddc
khushbooc13 <46244514+khushbooc13@users.noreply.github.com>
2019-03-06 12:52:29 +0530

    fix(Bug):Responsive
    fix(Bug):Responsive

--- src/Weather.js
+++ src/Weather.js
@@ -1,58 +1,88 @@
-import React, { Component } from 'react';
-import cloud1 from './cloud1.png';
-import rain from './rain.png';
-import './style.css';
+import React, { Component } from "react";
+import cloud1 from "./cloud1.png";
+import rain from "./rain.png";
+import "./style.css";
 
-class Weather extends Component
-{
-    setImage=(param)=>{
-        switch(param){
-            case 'Haze':
-                return {cloud1};
-                break;
-            default:
-                return {rain};
-                break;
-        }
-        
+class Weather extends Component {
+  setImage = param => {
+    switch (param) {
+      case "Haze":
+        return { cloud1 };
+        break;
+      default:
+        return { rain };
+        break;
     }
-    setDate=()=>{
-        var d=this.props.data.dt;
-        var date= new Date(d*1000);
-        var months = ['January','Febuary','March','April','May','June','July','August','September','October','November','December'];
-        var days=['Sunday','Monday','Tuesday','Wenesday','Thursday','Friday','Saturday'];
-        var fullDay=days[date.getDay()];
-        var fullMonth=months[date.getMonth()];
-        var fullYear=date.getFullYear();
-        var numberDay=date.getDate();
-        var displayDate=fullDay+", "+fullMonth+" "+numberDay+", "+fullYear;
-        return displayDate;
-
-    }
-    componentDidMount()
-    {
-        this.setImage();
-        this.setDate();
-    }
-    render()
-    { 
-        console.log(this.setImage('Haze'));
-       // console.log(this.props.data.weather ? this.props.data.weather[0].main:"null");   
-        return (
-
-            <div className="display-flex">
-                <div className="current">
-                    <h1 className="location">{this.props.data.name}</h1>
-                    <h2 className="date">{this.setDate()}</h2>
-                    <div className="weatherIcon"><img src={this.setImage(this.props.data.weather ? this.props.data.weather[0].main:"")} width="150" height="150"/>
-                        <p className="temprature">{this.props.data.main ? Math.floor(this.props.data.main.temp):""}&#x2103;</p>
-                        <p className="conditions">{this.props.data.weather ? this.props.data.weather[0].main:""}</p>
-                        <hr></hr>
-		            </div>
-                </div>
-            </div>
-        )
-    }
+  };
+  setDate = () => {
+    var d = this.props.data.dt;
+    var date = new Date(d * 1000);
+    var months = [
+      "January",
+      "Febuary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    var days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wenesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    var fullDay = days[date.getDay()];
+    var fullMonth = months[date.getMonth()];
+    var fullYear = date.getFullYear();
+    var numberDay = date.getDate();
+    var displayDate =
+      fullDay + ", " + fullMonth + " " + numberDay + ", " + fullYear;
+    return displayDate;
+  };
+  componentDidMount() {
+    this.setImage();
+    this.setDate();
+  }
+  render() {
+    console.log(this.setImage("Haze"));
+    // console.log(this.props.data.weather ? this.props.data.weather[0].main:"null");
+    return (
+      <div className="display-flex">
+        <div className="current">
+          <h1 className="location">{this.props.data.name}</h1>
+          <h2 className="date">{this.setDate()}</h2>
+          <div className="weatherIcon">
+            <img
+              src={this.setImage(
+                this.props.data.weather ? this.props.data.weather[0].main : ""
+              )}
+              width="150"
+              height="150"
+            />
+            <p className="temprature">
+              {this.props.data.main
+                ? Math.floor(this.props.data.main.temp)
+                : ""}
+              &#x2103;
+            </p>
+            <p className="conditions">
+              {this.props.data.weather ? this.props.data.weather[0].main : ""}
+            </p>
+            <hr />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Weather;
--- src/containers/WeatherForecast.js
+++ src/containers/WeatherForecast.js
@@ -3,38 +3,38 @@ import axios from "axios";
 import Weather from "../components/Weather.js";
 
 class WeatherForecast extends React.Component {
-    state = {
-        weatherData: [],
-        loaded: false
-    };
+  state = {
+    weatherData: [],
+    loaded: false
+  };
 
-    componentDidMount() {
-        this.getWeather();
-    }
+  componentDidMount() {
+    this.getWeather();
+  }
 
-    /*get data from api*/
-    getWeather = () => {
-        const url =
-            "http://api.openweathermap.org/data/2.5/weather?q=Mumbai&APPID=4678f6154011969601d17681f720023a&&units=metric";
-        axios
-            .get(url)
-            .then(response => {
-                this.setState({
-                    weatherData: response.data
-                });
-                this.setState({ loaded: true });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    };
+  /*get data from api*/
+  getWeather = () => {
+    const url =
+      "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&APPID=4678f6154011969601d17681f720023a&&units=metric";
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          weatherData: response.data
+        });
+        this.setState({ loaded: true });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
-    render() {
-        return !this.state.loaded ? (
-            <h1>Loading...</h1>
-        ) : (
-            <Weather data={this.state.weatherData} />
-        );
-    }
+  render() {
+    return !this.state.loaded ? (
+      <h1>Loading...</h1>
+    ) : (
+      <Weather data={this.state.weatherData} />
+    );
+  }
 }
 export default WeatherForecast;
