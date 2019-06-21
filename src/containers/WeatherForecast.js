91128dbc896abe1d6f080dedfa958ef50c363971
khushbooc13 <khushi.teddy13@gmail.com>
2019-06-21 13:26:11 +0530

    Merge branch 'master' of https://github.com/khushbooc13/react-weather-app
    # Conflicts:
    #	src/containers/WeatherForecast.js
    #	src/style.css

--- src/containers/WeatherForecast.js
+++ src/containers/WeatherForecast.js
@@ -2,7 +2,8 @@ import React from "react";
 import axios from "axios";
 import Weather from "../components/Weather.js";
 import Form from "../components/Form.jsx"
-import W from "../components/W"
+
+
 const API_KEY = '4678f6154011969601d17681f720023a';
 
 class WeatherForecast extends React.Component {
@@ -56,38 +57,5 @@ class WeatherForecast extends React.Component {
 
         );
     }
-  state = {
-    weatherData: [],
-    loaded: false
-  };
-
-  componentDidMount() {
-    this.getWeather();
-  }
-
-  /*get data from api*/
-  getWeather = () => {
-    const url =
-      "https://api.openweathermap.org/data/2.5/weather?q=Mumbai&APPID=4678f6154011969601d17681f720023a&&units=metric";
-    axios
-      .get(url)
-      .then(response => {
-        this.setState({
-          weatherData: response.data
-        });
-        this.setState({ loaded: true });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
-  render() {
-    return !this.state.loaded ? (
-      <h1>Loading...</h1>
-    ) : (
-      <Weather data={this.state.weatherData} />
-    );
-  }
 }
 export default WeatherForecast;
